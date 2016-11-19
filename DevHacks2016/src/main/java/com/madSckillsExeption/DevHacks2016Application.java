2a81a167ba5ed2adb5901e3068ad68aaff556585
catalin <catalin.matei@esolutions.ro>
2016-11-19 10:36:48 +0200

    first commit

---
+++ DevHacks2016/src/main/java/com/madSckillsExeption/DevHacks2016Application.java
@@ -0,0 +1,12 @@
+package com.madSckillsExeption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevHacks2016Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevHacks2016Application.class, args);
+	}
+}
