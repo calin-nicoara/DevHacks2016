d37c037d20911b6ee8deee736c1d236ff5c4aeed
calin_nicoara <calin.nicoara@esolutions.ro>
2016-11-19 14:14:01 +0200

    Added test entity with rest data repository.

---
+++ DevHacks2016/src/main/java/com/madSckillsExeption/entities/Test.java
@@ -0,0 +1,27 @@
+package com.madSckillsExeption.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EqualsAndHashCode
+public class Test {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    private String test;
+}
