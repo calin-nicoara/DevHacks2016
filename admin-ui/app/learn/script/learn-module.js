ff03e01f91018a9f8b8789c30e2aef5b51385ef9
catalin <catalin.matei@esolutions.ro>
2016-11-20 05:55:49 +0200

    join to a group

--- admin-ui/app/learn/script/learn-module.js
+++ admin-ui/app/learn/script/learn-module.js
@@ -1,40 +1,44 @@
 (function (angular) {
 
-    'use strict';
-    angular.module('MadSkillsDeveloper.learn', ['uiGmapgoogle-maps']).config(configure);
+  'use strict';
+  angular.module('MadSkillsDeveloper.learn', ['uiGmapgoogle-maps']).config(configure);
 
-    function configure($stateProvider) {
+  function configure($stateProvider) {
 
-        $stateProvider.state('learn', {
-            url: '/learn',
-            templateUrl: 'views/learn.html',
-            controller: 'LearnController as vm'
-        });
+    $stateProvider.state('learn', {
+      url: '/learn',
+      templateUrl: 'views/learn.html',
+      controller: 'LearnController as vm'
+    });
 
-        $stateProvider.state('selectSubject', {
-            url: '/selectSubject',
-            templateUrl: 'views/selectSubject.html',
-            controller: 'SelectSubjectController as vm'
-        });
+    $stateProvider.state('selectSubject', {
+      url: '/selectSubject',
+      templateUrl: 'views/selectSubject.html',
+      controller: 'SelectSubjectController as vm'
+    });
 
-        $stateProvider.state('studentStudyGroups', {
-            url: '/student/studyGroups',
-            templateUrl: 'views/studentStudyGroups.html',
-            controller: 'StudentStudyGroupsController as vm'
-        });
+    $stateProvider.state('studentStudyGroups', {
+      url: '/student/studyGroups',
+      templateUrl: 'views/studentStudyGroups.html',
+      controller: 'StudentStudyGroupsController as vm',
+      resolve: {
+        studyGroups: function (learnService) {
+          return learnService.getStudyGroupsForUser();
+        }
+      }
+    });
 
-        $stateProvider.state('learningResources', {
-            url: '/learningResources',
-            templateUrl: 'views/learningResources.html',
-            controller: 'LearningResourcesController as vm'
-        });
+    $stateProvider.state('learningResources', {
+      url: '/learningResources',
+      templateUrl: 'views/learningResources.html',
+      controller: 'LearningResourcesController as vm'
+    });
 
-        $stateProvider.state('learningResourcesJavascript', {
-          url: '/learningResourcesJavascript',
-          templateUrl: 'views/learningResourcesJavascript.html',
-          controller: 'LearningResourcesController as vm'
-        });
-    }
-
+    $stateProvider.state('learningResourcesJavascript', {
+      url: '/learningResourcesJavascript',
+      templateUrl: 'views/learningResourcesJavascript.html',
+      controller: 'LearningResourcesController as vm'
+    });
+  }
 
 }(angular));
