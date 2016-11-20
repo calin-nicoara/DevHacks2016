cdde65afe6d984736f962893ef63d6cdaa5de00b
ionut <ionut.pic@gmail.com>
2016-11-20 05:10:03 +0200

    selecting subjects

--- admin-ui/app/learn/script/selectSubject.controller.js
+++ admin-ui/app/learn/script/selectSubject.controller.js
@@ -19,8 +19,35 @@
 
         var userId = '1';
 
-        makeCall('GET', '/api/custom/subjects?userId=' + userId);
+        makeCall('GET', 'api/subjects').then(function(subjectsReply) {
+            makeCall('GET', '/api/custom/subjects?userId=' + userId).then(function(userSubjectsReply) {
 
+                vm.allSubjects = subjectsReply._embedded.subjects;
+                vm.userSubjects = userSubjectsReply;
+
+                var selectedMap = {};
+                _.forEach(vm.userSubjects, function (value,key) {
+                    selectedMap[value.id] = true;
+                });
+
+                _.forEach(vm.allSubjects, function (value,key) {
+                    if(selectedMap[value.id]) {
+                        value.isChecked = true;
+                    }
+                });
+
+            });
+        });
+
+        vm.submitSubjects = function() {
+            var checkedSubjectsMap = {};
+            _.forEach(vm.allSubjects, function (value,key) {
+                if(value.hasOwnProperty("isChecked")) {
+                    checkedSubjectsMap[value.id] = value.isChecked;
+                }
+            });
+            makeCall('POST', '/api/custom/subjects?userId=' + userId, checkedSubjectsMap);
+        };
         return vm;
     }
 
