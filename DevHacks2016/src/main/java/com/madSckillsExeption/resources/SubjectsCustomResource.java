9ded5a7b3bee56c631887c98c8c5f0c63ee1196c
ionut <ionut.pic@gmail.com>
2016-11-20 05:10:03 +0200

    selecting subjects

--- DevHacks2016/src/main/java/com/madSckillsExeption/resources/SubjectsCustomResource.java
+++ DevHacks2016/src/main/java/com/madSckillsExeption/resources/SubjectsCustomResource.java
@@ -3,16 +3,16 @@ package com.madSckillsExeption.resources;
 import com.madSckillsExeption.entities.StudentSubjectMap;
 import com.madSckillsExeption.repositories.StudentSubjectMapRepository;
 import com.madSckillsExeption.repositories.SubjectRestRepository;
+import com.madSckillsExeption.repositories.UserRestRepository;
 import com.madSckillsExeption.resources.models.UserSubjectModel;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
@@ -21,12 +21,15 @@ public class SubjectsCustomResource {
 
     private StudentSubjectMapRepository subjectMapRepository;
     private SubjectRestRepository subjectRestRepository;
+    private UserRestRepository userRestRepository;
 
     @Autowired
     public SubjectsCustomResource(StudentSubjectMapRepository subjectMapRepository,
-                                  SubjectRestRepository subjectRestRepository) {
+                                  SubjectRestRepository subjectRestRepository,
+                                  UserRestRepository userRestRepository) {
         this.subjectMapRepository = subjectMapRepository;
         this.subjectRestRepository = subjectRestRepository;
+        this.userRestRepository = userRestRepository;
     }
 
     @GetMapping
@@ -44,5 +47,22 @@ public class SubjectsCustomResource {
                 .collect(Collectors.toList());
 
         return ResponseEntity.ok(userSubjectModels);
+    }
+
+    @PostMapping
+    @Transactional
+    public void saveUserSubjects(@RequestParam("userId") Long userId, @RequestBody Map<Long, Boolean> selectedSubjects) {
+
+        for(Long selectedSubjectId : selectedSubjects.keySet()) {
+            if(!subjectMapRepository.findByUserIdAndSubjectId(userId, selectedSubjectId).isEmpty() && !selectedSubjects.get(selectedSubjectId)) {
+                subjectMapRepository.deleteByUserIdAndSubjectId(userId, selectedSubjectId);
+            } else if (subjectMapRepository.findByUserIdAndSubjectId(userId, selectedSubjectId).isEmpty() && selectedSubjects.get(selectedSubjectId)) {
+                StudentSubjectMap studentSubjectMap = new StudentSubjectMap();
+                studentSubjectMap.setUser(userRestRepository.getOne(userId));
+                studentSubjectMap.setSubject(subjectRestRepository.getOne(selectedSubjectId));
+                studentSubjectMap.setSkillLevel(0L);
+                subjectMapRepository.save(studentSubjectMap);
+            }
+        }
     }
 }
